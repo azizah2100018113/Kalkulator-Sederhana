8ef6ea87ea855d5fe945188ce0044cbbc0f48e0c
unknown <azizahrosyida12@gmail.com>
2022-03-19 20:19:40 +0700

    Kalkulator bag. 1

---
+++ kalkulator bag.1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int penambahan (int a, int b)
+{
+	int c=a+b;
+	return c;
+}
+int pengurangan (int d, int e)
+{
+	int f=d-e;
+	return f;
+}
+int perkalian (int g, int h)
+{
+	int i=g*h;
+	return i;
+}
+double pembagian (double j, double k)
+{
+	double l=j/k;
+	return l;
+}
+int pangkat(int m, int n)
+{
+	int o=pow(m, n);
+	return o;
+}
+double akarpangkat(double p)
+{
+	double q=sqrt(p);
+	return q;
+}
+
